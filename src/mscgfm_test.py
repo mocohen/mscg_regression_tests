4bef3b7cf195a34c092cf74b2a044d8a98bede79
Morris Cohen <mocohen@uchicago.edu>
2017-07-13 14:03:01 -0500

    First commit

---
+++ src/mscgfm_test.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+def md_content_equality(dat_1,dat_2, prefix="Data File equality: ",xyz_abs_tol=1e-8):
+    result = True
+    
+    return result
+
+
+
+
+def check_result_to_exitval(result):
+    '''Transforms boolean to command line exit value. 
+    True -> 0, False -> 1. No guard logic.
+    '''
+
+    return int(not(result))
+
